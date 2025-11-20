eaf86399e5e672fa48be250b5ec8af92be4a0437
Anton Zorkin <zanton1978@gmail.com>
2025-11-20 14:17:09 +0100

    commit message

--- src/server.js
+++ src/server.js
@@ -1,9 +1,31 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
 
 dotenv.config();
 const app = express();
-const PORT = 3000;
+app.use(cors());
+app.use(express.json());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+const PORT = process.env.PORT || 3000;
+
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
 });
@@ -12,6 +34,22 @@ app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
