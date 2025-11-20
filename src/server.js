0de156805dfedc5733b0f680d2f6c0747e36a1c5
Anton Zorkin <zanton1978@gmail.com>
2025-11-20 10:13:12 +0100

    commit message

--- src/server.js
+++ src/server.js
@@ -1,2 +1,17 @@
 import dotenv from 'dotenv';
+import express from 'express';
+
 dotenv.config();
+const app = express();
+const PORT = 3000;
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
