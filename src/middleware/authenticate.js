98acdf26887658b54ed3d2ae26df790fb53ec7de
Anton Zorkin <zanton1978@gmail.com>
2025-12-11 12:23:18 +0100

    task4

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,5 @@
+// import createHttpError from 'http-errors';
+// import { Session } from '../models/session.js';
+// import { User } from '../models/user.js';
+
+// export const
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
