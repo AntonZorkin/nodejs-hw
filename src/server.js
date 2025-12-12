c31027bccaec4ca9a26a08b4bd9eb034a2963388
Anton Zorkin <zanton1978@gmail.com>
2025-12-12 10:40:30 +0100

    commit message

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
@@ -15,6 +16,7 @@ await connectMongoDB();
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000;
 
