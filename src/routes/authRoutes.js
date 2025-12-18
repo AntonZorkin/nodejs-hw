e0aabf301a20942aebcd1302fbeafd7467a0f119
Anton Zorkin <zanton1978@gmail.com>
2025-12-18 12:31:06 +0100

    Task 5

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,12 +3,16 @@ import { celebrate } from 'celebrate';
 import {
   loginUserSchema,
   registerUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
 } from '../validations/authValidation.js';
 import {
   loginUser,
   logoutUser,
   refreshUserSession,
   registerUser,
+  requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
 
 const router = Router();
@@ -18,4 +22,15 @@ router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 
 router.post('/auth/refresh', refreshUserSession);
+
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
 export default router;
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,4 @@
+// import { Router } from 'express';
+// import { celebrate } from 'celebrate';
+
+// const router = Router();
